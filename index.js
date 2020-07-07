1c13002f5377c1048a6bc2a9798a8310b15c258d
Julio Carrion <a20161448@pucp.edu.pe>
2020-07-06 19:17:28 -0500

    anthony callate2

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "inventariotest"
+});
+
+conn.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Conexión exitosa a base de datos");
+    }
+});
