3929150e79aa84f51b1c6f9c53992e7b69fd6f57
Samuel-Ayala <a20166262@pucp.edu.pe>
2020-07-06 19:40:47 -0500

    asdasd

--- index.js
+++ index.js
@@ -21,3 +21,48 @@ conn.connect(function (err) {
         console.log("Conexión exitosa a base de datos");
     }
 });
+
+//localhost:8080/categoriasEquipo/get/{id}
+app.get("/categoriasEquipo/get/:id",function (request,response) {
+    var id = request.params.id;
+    var query1 = "select * from categoriaequipo";
+    var query2 = "select * from categoriaequipo c where c.idCategoriaEquipo = ?";
+    var parametro = id;
+
+    if(id == null){
+        conn.query(query1,function (err,resultado) {
+            if (err){
+                console.log(err);
+            }else {
+                response.json(resultado);
+            }
+        })
+    }else {
+        conn.query(query2,parametro,function (err,resultado) {
+            if (err){
+                console.log(err);
+            }else {
+                response.json(resultado);
+            }
+        })
+    }
+
+})
+
+app.listen(8080,function () {
+    console.log("servidor levantado exitosamente");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
